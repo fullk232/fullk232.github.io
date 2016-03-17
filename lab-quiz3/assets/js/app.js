fd4307310b90479fad6cf0e86b982751e1ca5503
katie <fullk232@newschool.edu>
2016-03-17 10:54:05 -0400

    quiz3 first

---
+++ lab-quiz3/assets/js/app.js
@@ -0,0 +1,35 @@
+console.log("hello world");
+var Color = "green";
+var wallet = 1100;
+var converted_wallet = 10;
+
+var conversion_rate = [0.88, 111.58, 0.69];
+console.log(conversion_rate[1]);
+
+
+
+function convert_currency(amount, converter){
+	var converted_wallet = 968;
+}
+console.log(1100 * 0.88)
+
+
+
+function GetValueEuro()
+{
+	var Euro= new Array(0.88, 0.88);
+    document.getElementById("message").innerHTML=0.88;
+}
+function GetValueYen()
+{
+	var Yen= new Array(111.58, 111.58);
+    document.getElementById("message2").innerHTML=111.58;
+}
+function GetValueGBP()
+{
+	var GBP= new Array(0.69, 0.69);
+    //alert(random);
+    document.getElementById("message3").innerHTML=0.69;
+}
+
+
---
+++ practicejava/app.js
@@ -0,0 +1,49 @@
+//confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!');
+//prompt("Who are you?");//
+console.log("hello world");
+console.log("hello 'my' name \"is\" katie");
+
+"I'm coding like a champ".length>"Katie".length
+console.log(2 * 5)
+
+console.log("Xiao Hui".length < 122);
+console.log("Goody Donaldson".length > 8);
+console.log(8*2 === 16);
+console.log(8*2 <= 16);
+console.log(8*2 >= 16);
+console.log(8*2 !== 14);
+
+if ( "Katie".length === 5 ) {
+    console.log("You're dumb");
+}
+
+if ( "You".length === 3 ) 
+{
+    console.log("Let's go down the first road!");
+}
+else 
+{
+    // What should we do if the condition is false? Fill in here: 
+    console.log("I love you")
+    
+}
+
+if (12 / 4 === "Ari".length) {
+    confirm("Will this run the first block?");
+} else {
+    confirm("Or the second block?");
+}
+
+
+console.log(14 % 3)
+console.log(99 % 8)
+console.log(11 % 3)
+console.log("January".substring(0,3));
+console.log("Melbourne is great".substring(0,12));
+console.log("Hamburgers".substring(3,10));
+
+
+var myAge = 19;
+console.log(myAge);19;
+
+
