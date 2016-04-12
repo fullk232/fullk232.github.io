b3618e693b1cb1ea72f9536259017e344a556cf7
katie <fullk232@newschool.edu>
2016-04-12 02:13:20 -0400

    typeface

---
+++ timemag/assets/js/app.js
@@ -0,0 +1,20 @@
+showImg = function()
+{
+    x=document.getElementById("map_img")
+    x.style.visibility="visible";
+}
+showImg2 = function()
+{
+    x=document.getElementById("map_img2")
+    x.style.visibility="visible";
+}
+showImg3 = function()
+{
+    x=document.getElementById("map_img3")
+    x.style.visibility="visible";
+}
+showImg4 = function()
+{
+    x=document.getElementById("map_img4")
+    x.style.visibility="visible";
+}
