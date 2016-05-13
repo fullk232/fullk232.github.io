bcc50a30ed923be7a5d07c75c1ff96cd2c57095c
katie <fullk232@newschool.edu>
2016-05-12 21:07:12 -0400

    poem generator v1

--- fortuneteller/assets/js/app.js
+++ fortuneteller/assets/js/app.js
@@ -1,11 +1,8 @@
 var objects = ["it's not impossible </br>", "hmm... i'd say so</br>", "lol no</br>", "don't get your hopes up but it's possible</br>", "100%</br>", "the future is looking bright</br>",
-				"idk what do u think</br>", "i don't understand the question</br>", "could happen</br>", "congratulations!!</br>", "sorry but it's not looking good</br>", "come back later</br>"];
-
-var colors = ['red', 'blue', 'yellow'];
+				"idk what do u think</br>", "i don't understand the question</br>", "could happen</br>"];
 
 $('.button').click(function() {
 	var random_object_number = Math.floor(Math.random() * objects.length),
-		random_color_number = Math.floor(Math.random() * colors.length),
 		object_to_use		= objects[random_object_number],
 		color_to_use		= colors[random_color_number],
 		result				= '<span class="object ' + color_to_use +  '">' + object_to_use + '</span>';
---
+++ todo/assets/js/app.js
@@ -0,0 +1,89 @@
+var words = new Array("the", "of", "and", "a", "to", "in", "is", "you", "that", "it", "he", "was", "for", "on", "are", "as", "with", "his", "they", "I", "at", "be", "this", "have", "from", "or", "one", "had", "by", "words", "but", "not", "what", "all", "were", "we", "when", "your", "can", "said", "there", "use", "an", "each", "which", "she", "do", "how", "their", "if", "will", "up", "other", "about", "out", "many", "then", "them", "these", "so", "some", "her", "would", "make", "like", "him", "into", "time", "has", "look", "two", "more", "write", "go", "see", "number", "no", "way", "could", "people", "my", "than", "first", "water", "been", "called", "who", "oil", "sit", "now", "find", "long", "down", "day", "did", "get", "come", "made", "may", "part", "over", "new", "sound", "take", "only", "little", "work", "know", "place", "years", "live", "me", "back", "give", "most", "very", "after", "things", "our", "just", "name", "good", "sentence", "man", "think", "say", "great", "where", "help", "through", "much", "before", "line", "right", "too", "means", "old", "any", "same", "tell", "boy", "follow", "came", "want", "show", "also", "around", "form", "three", "small", "set", "put", "end", "does", "another", "well", "large", "must", "big", "even", "such", "because", "turn", "here", "why", "ask", "went", "men", "read", "need", "land", "different", "home", "us", "move", "try", "kind", "hand", "picture", "again", "change", "off", "play", "spell", "air", "away", "animal", "house", "point", "page", "letter", "mother", "answer", "found", "study", "still", "learn", "should", "America", "world", "high", "every", "near", "add", "food", "between", "own", "below", "country", "plant", "last", "school", "father", "keep", "tree", "never", "start", "city", "earth", "eyes", "light", "thought", "head", "under", "story", "saw", "left", "don&rsquo;t", "few", "while", "along", "might", "close", "something", "seem", "next", "hard", "open", "example", "begin", "life", "always", "those", "both", "paper", "together", "got", "group", "often", "run", "important", "until", "children", "side", "feet", "car", "mile", "night", "walk", "white", "sea", "began", "grow", "took", "river", "four", "carry", "state", "once", "book", "hear", "stop", "without", "second", "late", "miss", "idea", "enough", "eat", "face", "watch", "far", "Indian", "real", "almost", "let", "above", "girl", "sometimes", "mountains", "cut", "young", "talk", "soon", "list", "song", "being", "leave", "family", "it&rsquo;s", "body", "music", "color", "stand", "sun", "questions", "fish", "area", "mark", "dog", "horse", "birds", "problem", "complete", "room", "knew", "since", "ever", "piece", "told", "usually", "didn&rsquo;t", "friends", "easy", "heard", "order", "red", "door", "sure", "become", "top", "ship", "across", "today", "during", "short", "better", "best", "however", "low", "hours", "black", "products", "happened", "whole", "measure", "remember", "early", "waves", "reached", "listen", "wind", "rock", "space", "covered", "fast", "several", "hold", "himself", "toward", "five", "step", "morning", "passed", "vowel", "true", "hundred", "against", "pattern", "numeral", "table", "north", "slowly", "money", "map", "farm", "pulled", "draw", "voice", "seen", "cold", "cried", "plan", "notice", "south", "sing", "war", "ground", "fall", "king", "town", "I&rsquo;ll", "unit", "figure", "certain", "field", "travel", "wood", "fire", "upon", "done", "English", "road", "half", "ten", "fly", "gave", "box", "finally", "wait", "correct", "oh", "quickly", "person", "became", "shown", "minutes", "strong", "verb", "stars", "front", "feel", "fact", "inches", "street", "decided", "contain", "course", "surface", "produce", "building", "ocean", "class", "note", "nothing", "rest", "carefully", "scientists", "inside", "wheels", "stay", "green", "known", "island", "week", "less", "machine", "base", "ago", "stood", "plane", "system", "behind", "ran", "round", "boat", "game", "force", "brought", "understand", "warm", "common", "bring", "explain", "dry", "though", "language", "shape", "deep", "thousands", "yes", "clear", "equation", "yet", "government", "filled", "heat", "full", "hot", "check", "object", "am", "rule", "among", "noun", "power", "cannot", "able", "six", "size", "dark", "ball", "material", "special", "heavy", "fine", "pair", "circle", "include", "built", "can&rsquo;t", "matter", "square", "syllables", "perhaps", "bill", "felt", "suddenly", "test", "direction", "center", "farmers", "ready", "anything", "divided", "general", "energy", "subject", "Europe", "moon", "region", "return", "believe", "dance", "members", "picked", "simple", "cells", "paint", "mind", "love", "cause", "rain", "exercise", "eggs", "train", "blue", "wish", "drop", "developed", "window", "difference", "distance", "heart", "site", "sum", "summer", "wall", "forest", "probably", "legs", "sat", "main", "winter", "wide", "written", "length", "reason", "kept", "interest", "arms", "brother", "race", "present", "beautiful", "store", "job", "edge", "past", "sign", "record", "finished", "discovered", "wild", "happy", "beside", "gone", "sky", "grass", "million", "west", "lay", "weather", "root", "instruments", "meet", "third", "months", "paragraph", "raised", "represent", "soft", "whether", "clothes", "flowers", "shall", "teacher", "held", "describe", "drive", "cross", "speak", "solve", "appear", "metal", "son", "either", "ice", "sleep", "village", "factors", "result", "jumped", "snow", "ride", "care", "floor", "hill", "pushed", "baby", "buy", "century", "outside", "everything", "tall", "already", "instead", "phrase", "soil", "bed", "copy", "free", "hope", "spring", "case", "laughed", "nation", "quite", "type", "themselves", "temperature", "bright", "lead", "everyone", "method", "section", "lake", "iron", "within", "dictionary", "hair", "age", "amount", "scale", "pounds", "although", "per", "broken", "moment", "tiny", "possible", "gold", "milk", "quiet", "natural", "lot", "stone", "act", "build", "middle", "speed", "count", "consonant", "someone", "sail", "rolled", "bear", "wonder", "smiled", "angle", "fraction", "Africa", "killed", "melody", "bottom", "trip", "hole", "poor", "let&rsquo;s", "fight", "surprise", "French", "died", "beat", "exactly", "remain", "dress", "cat", "couldn&rsquo;t", "fingers", "row", "least", "catch", "climbed", "wrote", "shouted", "continued", "itself", "else", "plains", "gas", "England", "burning", "design", "joined", "foot", "law", "ears", "glass", "you&rsquo;re", "grew", "skin", "valley", "cents", "key", "president", "brown", "trouble", "cool", "cloud", "lost", "sent", "symbols", "wear", "bad", "save", "experiment", "engine", "alone", "drawing", "east", "choose", "single", "touch", "information", "express", "mouth", "yard", "equal", "decimal", "yourself", "control", "practice", "report", "straight", "rise", "statement", "stick", "party", "seeds", "suppose", "woman", "coast", "bank", "period", "wire", "pay", "clean", "visit", "bit", "whose", "received", "garden", "please", "strange", "caught", "fell", "team", "God", "captain", "direct", "ring", "serve", "child", "desert", "increase", "history", "cost", "maybe", "business", "separate", "break", "uncle", "hunting", "flow", "lady", "students", "human", "art", "feeling", "supply", "corner", "electric", "insects", "crops", "tone", "hit", "sand", "doctor", "provide", "thus", "won&rsquo;t", "cook", "bones", "mall", "board", "modern", "compound", "mine", "wasn&rsquo;t", "fit", "addition", "belong", "safe", "soldiers", "guess", "silent", "trade", "rather", "compare", "crowd", "poem", "enjoy", "elements", "indicate", "except", "expect", "flat", "seven", "interesting", "sense", "string", "blow", "famous", "value", "wings", "movement", "pole", "exciting", "branches", "thick", "blood", "lie", "spot", "bell", "fun", "loud", "consider", "suggested", "thin", "position", "entered", "fruit", "tied", "rich", "dollars", "send", "sight", "chief", "Japanese", "stream", "planets", "rhythm", "eight", "science", "major", "observe", "tube", "necessary", "weight", "meat", "lifted", "process", "army", "hat", "property", "particular", "swim", "terms", "current", "park", "sell", "shoulder", "industry", "wash", "block", "spread", "cattle", "wife", "sharp", "company", "radio", "we&rsquo;ll", "action", "capital", "factories", "settled", "yellow", "isn&rsquo;t", "southern", "truck", "fair", "printed", "wouldn&rsquo;t", "ahead", "chance", "born", "level", "triangle", "molecules", "France", "repeated", "column", "western", "church", "sister", "oxygen", "plural", "various", "agreed", "opposite", "wrong", "chart", "prepared", "pretty", "solution", "fresh", "shop", "suffix", "especially", "shoes", "actually", "nose", "afraid", "dead", "sugar", "adjective", "fig", "office", "huge", "gun", "similar", "death", "score", "forward", "stretched", "experience", "rose", "allow", "fear", "workers", "Washington", "Greek", "women", "bought", "led", "march", "northern", "create", "British", "difficult", "match", "win", "doesn&rsquo;t", "steel", "total", "deal", "determine", "evening", "hoe", "rope", "cotton", "apple", "details", "entire", "corn", "substances", "smell", "tools", "conditions", "cows", "track", "arrived", "located", "sir", "seat", "division", "effect", "underline", "view");
+
+
+
+var values = [];
+var currentImport = false;
+
+$('body').on('click', '.button', function() {
+  $('.todo').prepend(' <div class="unit"><h3>Task</h3><div class="input"><input type="text" value="" class="myinput"></div><h4>Importance</h4><div class="importance"><p data-button="one" class="number">1</p><p data-button="two" class="number">2</p><p data-button="three" class="number">3</p><p data-button="four" class="number">4</p><p data-button="five" class="number">5</p></div><p data-button="finish" class="finish">Finish</p></div>');
+  values.push($($('.myinput')[1]).val());
+  console.log(values);
+});
+
+
+$('body').on('click', '.number', function() {
+	$(this).parent().children().removeClass('clicked');
+  	$(this).toggleClass('clicked');
+
+  	var button_name = $(this).data('button');
+  	currentImport = $(this).data('button');
+
+  	if (button_name == "one")
+  		$('.results').addClass('one');
+
+  	if (button_name == "two")
+  		$('.results').addClass('two');
+
+  	if (button_name == "three")
+  		$('.results').addClass('three');
+
+  	if (button_name == "four")
+  		$('.results').addClass('four');
+
+  	if (button_name == "five")
+  		$('.results').addClass('five');
+
+
+});
+
+
+$('body').on('click', '.finish', function() {
+	var newString = "";
+	console.log(values);
+
+	for(var z=0; z<values.length; z++) {
+
+		var str = values[z];
+		var res = str.split(" ");
+
+		for (var increment = 0; increment < res.length; increment++) {
+			var myArray = [];
+			for (var i=0; i<words.length; i++) {
+			  	if (words[i].substr(0,2) == res[increment].substr(0,2)) {
+			    	console.log(words[i]);
+			    	myArray.push(words[i]);
+			  	}
+			}
+			
+			newString = newString + myArray[Math.floor(Math.random()*myArray.length)] + " ";
+			values = [];
+		}
+
+
+	}
+
+	console.log(newString);
+	$(".finishedpoem").append("<span class='" + currentImport + "'>" + newString + "</span>" + "<br>");
+	
+
+
+	$('.finish').removeClass('active');
+	$(this).addClass('active');
+	
+	var button_name = $(this).data('button');
+	
+	$('.content').attr('class', 'column content');
+	$('.content').addClass(button_name);
+
+	if (button_name == 'finish')
+	$('.poem').removeClass('hide');
+});
+
+
+$('.close').click(function() {
+	$('.poem').addClass('hide');
+	$('.finish').removeClass('active');
+
+});
+ 
